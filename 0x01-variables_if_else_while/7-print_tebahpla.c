0ef3b7aad26dd8dd34beeb00ba797305a5de9768
amos-olayinka <amosolayinka92@gmail.com>
2022-03-14 22:28:27 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char z = 'z';
+
+	for (z = 'z'; z >= 'a'; z--)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
