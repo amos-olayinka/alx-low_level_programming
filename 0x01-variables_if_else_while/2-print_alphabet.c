86b6292400de589f45efba7a19c5c0dddd013479
amos-olayinka <amosolayinka92@gmail.com>
2022-03-14 22:00:59 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -Determines if a number is positive or zero.
+ * main - Determines if a number is positive, negative or zero.
  *
  * Return: Always 0
  */
@@ -15,40 +15,5 @@ int main(void)
 		putchar(a);
 	}
 	putchar('\n');
-
-TOFUNMI@DESKTOP-AVFAO7J MINGW64 ~ (main)
-$ ssh 4cc4e502fd23@4cc4e502fd23.96477629.alx-cod.online
-4cc4e502fd23@4cc4e502fd23.96477629.alx-cod.online's password:
-root@4cc4e502fd23:/# cd root
-root@4cc4e502fd23:~# ls
-0-current_working_directory  0X02_vi                    school
-0-hello_world                0x03_vi                    school_is_amazing
-0x00-shell_basics            alx-low_level_programming  school_ls_amazing
-0x01_emacs                   alx-zero_day               so
-0x02_emacs                   Betty                      so_cool
-0x02-shell_redirections      new_school
-0x02_vi                      README.md
-root@4cc4e502fd23:~# cd alx-low_level_programming
-root@4cc4e502fd23:~/alx-low_level_programming# cd 0x01-variables_if_else_while
-root@4cc4e502fd23:~/alx-low_level_programming/0x01-variables_if_else_while# touch 2-print_alphabet.c
-root@4cc4e502fd23:~/alx-low_level_programming/0x01-variables_if_else_while# cat 2-print_alphabet.c
-root@4cc4e502fd23:~/alx-low_level_programming/0x01-variables_if_else_while# cat > 2-print_alphabet.c
-#include <stdio.h>
-
-/**
- * main -Determines if a number is positive or zero.
- *
- * Return: Always 0
- */
-
-int main(void)
-{
-        char a = 'a';
-
-        for (a = 'a'; a <= 'z'; a++)
-        {
-                putchar(a);
-        }
-        putchar('\n');
 	return (0);
 }
